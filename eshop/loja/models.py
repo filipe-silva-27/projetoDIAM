438009e862add4dbf500a18dca8e5d567f9f1ccc
Filipe Silva <filipesprogramming@gmail.com>
2022-05-04 14:46:43 +0100

    models
    produto tem: preco nome e descricao, é ou n personalizavel, thumbnail e um vendedor associado. Comentario sobre um produto por um User. Vendedor é generalização de User mas a Telma vê se falta mais algo

--- eshop/loja/models.py
+++ eshop/loja/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Produto(models.Model):
+    preco = models.IntegerField()
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=300)
+    personalizavel = models.BooleanField(default=False)
+    pic = models.ImageField(upload_to='images', blank=True, null=True)
+    seller = models.ForeignKey(Vendedor, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.nome
+class Vendedor(models.Model):
+    who = models.OneToOneField(User, on_delete=models.CASCADE) #generalizacao
+    
+
+class Comentario(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
