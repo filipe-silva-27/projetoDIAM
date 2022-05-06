1e542032f64813a81ccf5a6559ce744222e11ea8
Filipe Silva <filipesprogramming@gmail.com>
2022-05-06 12:30:17 +0100

    models e comecei a passar as coisas para loja.html

--- eshop/loja/models.py
+++ eshop/loja/models.py
@@ -13,10 +13,10 @@ class Vendedor(models.Model):
 class Produto(models.Model):
     preco = models.IntegerField()
     nome = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=300)
-    personalizavel = models.BooleanField(default=False)
+    """ descricao = models.CharField(max_length=300) """
+    """ personalizavel = models.BooleanField(default=False)
     pic = models.ImageField(upload_to='images', blank=True, null=True)
-    seller = models.ForeignKey(Vendedor, on_delete=models.RESTRICT)
+    seller = models.ForeignKey(Vendedor, on_delete=models.RESTRICT) """
 
     def __str__(self):
         return self.nome
@@ -29,6 +29,8 @@ class Comentario(models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     def __str__(self):
         return self.user.username + ' disse: ' + self.text
+
+
 class Questao(models.Model):
     texto = models.CharField(max_length=200)
     """ pub_data = models.DateTimeField('data de publicacao') """
@@ -45,22 +47,15 @@ class QuestaoDoProduto(models.Model): #primary key composta
     questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
      
 class Opcao(models.Model):
-    questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
+    opcao_questao = models.ForeignKey(QuestaoDoProduto, on_delete=models.CASCADE)
     opcao_texto = models.CharField(max_length=200)
-    votos = models.IntegerField(default=0)
 
     def __str__(self):
         return self.opcao_texto
 
-class OpcaoDaQuestaoProd(models.Model): #aqui é primary key composta, é uma ternária?
+""" class Pedido(models.Model):
     questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
     opcao = models.ForeignKey(Opcao, on_delete=models.CASCADE)
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-
-class Pedido(models.Model):
-    questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
-    opcao = models.ForeignKey(Opcao, on_delete=models.CASCADE)
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nas views por produto = getObjectOR404(Produto, on_delete=models.CASCADE) 
     cliente = models.ForeignKey(User, on_delete=models.CASCADE)
-##aloaloaloalaolaolaoaloalaolaoaloaloalaolaoloaloalaolaoalol
-
+ """
--- eshop/loja/views.py
+++ eshop/loja/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import *
 
 def index(request):
     return  HttpResponse(render(request, 'loja/main.html' ))
 
 def loja(request):
-    return  render(request, 'loja/loja.html' )
+    produtos = Produto.objects.all()
+    prod1 = Produto(id=1, preco=40, nome="gar")
+    prod2 = Produto(id=2, preco=50, nome="dois")
+    lista_teste = {prod1, prod2}
+    return render(request, 'loja/loja.html' , {'products_list': lista_teste})
 
 def cart(request):
-    return  render(request, 'loja/cart.html' )
+    return render(request, 'loja/cart.html' )
 
 def checkout(request):
-    return  render(request, 'loja/checkout.html')
+    return render(request, 'loja/checkout.html')
+
+def detalheProd (request, produto_id):
+    return render(request, 'loja/detalhesProd.html' , {'produto': produto_id})
